51ff56df24b364e4aad3edd21abea32f065aa17e
gabrielgsv <gabriel.soares.vilela@gmail.com>
2019-11-22 16:30:20 -0300

    add debug

--- components/sideNav/index.js
+++ components/sideNav/index.js
@@ -12,17 +12,17 @@ export default () => {
             <Nav>
               <Link to="/">
                 <Nav.Item eventKey="1" icon={<Icon size="3x" icon="home" />}>
-                  {expand === true && "Home"}
+                  Home
                 </Nav.Item>
               </Link>
               <Link to="/about">
                 <Nav.Item eventKey="2" icon={<Icon icon="info" />}>
-                  {expand === true && "About"}
+                  About
                 </Nav.Item>
               </Link>
               <Link to="/users">
                 <Nav.Item eventKey="3" icon={<Icon icon="user" />}>
-                  {expand === true && "User"}
+                  User
                 </Nav.Item>
               </Link>
             </Nav>
